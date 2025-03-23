f56d9981a0da71959f2bdf4685c51a8dabd9eca9
NataMarques08 <nmolinamarques@gmail.com>
2025-03-23 19:07:41 -0300

    Documentation is complete

--- src/main/java/com/nata/empresa/controller/EmployeeController.java
+++ src/main/java/com/nata/empresa/controller/EmployeeController.java
@@ -1,18 +1,16 @@
 package com.nata.empresa.controller;
 
 import com.nata.empresa.data.dto.EmployeeDTO;
-import com.nata.empresa.model.Employee;
 import com.nata.empresa.services.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -46,7 +44,6 @@ public class EmployeeController {
                     @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
                     @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
             }
-
     )
     @Transactional
     public ResponseEntity<EmployeeDTO> create(@RequestBody @Valid EmployeeDTO employeeDTO){
@@ -55,7 +52,20 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createEmployee);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping(
+            value = "/getAll",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(
+            summary = "List all employees in the company",
+            description = "This method lists all employees and returns a list of employees.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = EmployeeDTO.class)))),
+                    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+                    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+            }
+    )
     public ResponseEntity<List<EmployeeDTO>> findAll(){
         var employees = service.findAll();
         for (EmployeeDTO employeeDTO : employees) {
@@ -64,7 +74,20 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.OK).body(employees);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(
+            value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(
+            summary = "Show employee in the company by id",
+            description = "This method show employee and returns employee data by ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+                    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+            }
+    )
     public ResponseEntity<EmployeeDTO> findById(@PathVariable("id") Long id){
         EmployeeDTO employeeDTO = service.findById(id);
         addHateoasLink(id, employeeDTO);
@@ -73,7 +96,21 @@ public class EmployeeController {
 
 
 
-    @PutMapping("/update/{id}")
+    @PutMapping(
+            value = "/update/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(
+            summary = "Update an employee",
+            description = "This method update an employee by ID and return the employee",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Created", content = @Content),
+                    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+                    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+            }
+    )
     @Transactional
     public ResponseEntity<EmployeeDTO> update(@PathVariable("id") Long id,
                                               @RequestBody @Valid EmployeeDTO employeeDTO){
@@ -84,6 +121,16 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(
+            summary = "Delete employee",
+            description = "This method just delete an employee and return nothing",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+                    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+                    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+            }
+    )
     @Transactional
     public ResponseEntity<Void> delete(@PathVariable("id") Long id){
         service.delete(id);
